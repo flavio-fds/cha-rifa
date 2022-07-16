7d61ddfaaef9e97c24d28ddbe684fd47198e6d59
Flavio Fernandes <flaviofdst@gmail.com>
2022-07-16 12:42:38 -0300

    handle api error

--- pages/api/allNumbers.js
+++ pages/api/allNumbers.js
@@ -1,7 +1,13 @@
 import prisma from '../../lib/prisma'
 
 export default async function handle(req, res) {
-  const post = await prisma.number.findMany({
-  })
-  res.json(post)
+  try {
+    const post = await prisma.number.findMany({
+    })
+    res.json(post)
+  } catch (error) {
+    console.log(error);
+    res.json({ erro: 'erro' })
+  }
+
 }
